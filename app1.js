0366a2cdab99d68a66e064befa3140131642e8a3
moneyutkarsh <155374633+moneyutkarsh@users.noreply.github.com>
2026-04-02 12:32:53 +0530

    Fix typo in secret variable value

--- app1.js
+++ app1.js
@@ -3,4 +3,4 @@ const password = "admin123";
 const apiKey = "sk-secret-key-12345678";
 const db = require('mongodb');
 db.collection.find({user: req.body.username});
-const secret = "supersecret567734"
+const secret = "supsecret567734"
