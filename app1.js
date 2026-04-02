4844b0d139dba34a27ac36df2dfe2fa5c3dcb45e
moneyutkarsh <155374633+moneyutkarsh@users.noreply.github.com>
2026-04-02 10:21:02 +0530

    Update password and add API key and database connection

--- app1.js
+++ app1.js
@@ -1,1 +1,6 @@
-const password="admin1234";
+
+const password = "admin123";
+const apiKey = "sk-secret-key-12345";
+const db = require('mongodb');
+db.collection.find({user: req.body.username});
+const secret = "supersecret125778734"
