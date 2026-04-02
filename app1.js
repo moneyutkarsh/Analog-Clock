5a31acfa074928151737beeda71318a42dd45816
moneyutkarsh <155374633+moneyutkarsh@users.noreply.github.com>
2026-04-02 12:45:35 +0530

    Fix typo in apiKey variable assignment

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 
 const password = "admin123";
-const apiKey = "sk-secret-key-12345678";
+const apiKey = "sk-sectret-key-12345678";
 const db = require('mongodb');
 db.collection.find({user: req.body.username});
 const secret = "supsecret567734"
