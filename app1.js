018e980b91e7a926690c866fa984320e2335fe2b
moneyutkarsh <155374633+moneyutkarsh@users.noreply.github.com>
2026-04-02 12:09:44 +0530

    Update API key in app1.js

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 
 const password = "admin123";
-const apiKey = "sk-secret-key-12345";
+const apiKey = "sk-secret-key-12345678";
 const db = require('mongodb');
 db.collection.find({user: req.body.username});
 const secret = "supersecret12567778734"
